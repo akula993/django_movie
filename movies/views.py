11179bead679e8ab43f2f0a453d5a003272dcdc9
user <akula993@gmail.com>
2022-04-20 21:16:19 +0300

    first commit

---
+++ movies/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.base import View
+
+from .models import Movie
+
+
+class MoviesView(ListView):
+    '''Список фильмов'''
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    template_name = 'movies/movies.html'
+
+class MovieDetailView(DetailView):
+    ''' Полное описание Фильма '''
+    model = Movie
+    slug_field = 'url'
