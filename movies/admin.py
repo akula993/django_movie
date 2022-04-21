503d688bfcf520ecf75a924abdcecb71b15898f8
gavr <gv@doublev.ru>
2022-04-21 23:18:54 +0300

    saved it at work

---
+++ movies/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Catefory)
+admin.site.register(Genre)
+admin.site.register(Movie)
+admin.site.register(MoviesShots)
+admin.site.register(Actor)
+admin.site.register(Rating)
+admin.site.register(RatingStar)
+admin.site.register(Reviews)
