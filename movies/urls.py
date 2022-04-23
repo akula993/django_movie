d21c29dc6da9ef3a327c8dc33ce92411cea6a37e
user <akula993@gmail.com>
2022-04-23 22:41:02 +0300

    Перестала работать фильтрация нужно искать

--- movies/urls.py
+++ movies/urls.py
@@ -4,6 +4,8 @@ from .views import *
 urlpatterns = [
     path('', MoviesView.as_view()),
     path("filter/", FilterMoviesView.as_view(), name='filter'),
+    path("json-filter/", JsonFilterMoviesView.as_view(), name='json_filter'),
+
     path("<slug:slug>/", MovieDetailView.as_view(), name="movie_detail"),
     path("trview/<int:pk>/", AddReview.as_view(), name="add_review"),
     path("actor/<str:slug>/", ActorView.as_view(), name="actor_detail"),
